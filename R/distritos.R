6735e77790b500a0fce017ad678863da87b522fe
PaulESantos <paulefrens@gmail.com>
2024-02-12 20:23:01 -0500

    as

---
+++ R/distritos.R
@@ -0,0 +1,8 @@
+#' Distritos Peru
+#'
+#' Información geográfica vectorial de los 1722 distritos del PERU y su geometría.
+#'
+#' @source \url{https://ide.inei.gob.pe/}
+#' @format A \code{SpatialPolygonsDataFrame}
+#' @keywords dataset
+"distritos_tbl"
